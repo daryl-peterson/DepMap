94afd4c1167dfdc645d795c7ea1380e1d9d20782
Daryl Peterson <daryl.peterson@gmail.com>
2018-07-01 21:51:53 -0500

    Changes to be committed: 	modified:   DepMap.inc.php 	modified:   DepMap.php Untracked files: 	DepMapper.php

--- DepMap.php
+++ DepMap.php
@@ -8,13 +8,28 @@
  * option) any later version.  Please see LICENSE at the top level of
  * the source code distribution for details.
  */
- 
-class DepMap {
-	
-  public function menu() {
-    echo('<li><a href="plugin/?p='.get_class().'">Dependency Map</a></li>');
-  }
-  
-}
 
-?>
+class DepMap
+{
+
+    /**
+     * Create menu item
+     */
+    public function menu()
+    {
+        echo('<li><a href="plugin/?p='.get_class().'">Dependency Map</a></li>');
+        self::load();
+    }
+
+    /**
+     * Add PSR-4 path to composer
+     * @global type $config
+     */
+    private static function load(){
+        global $config;
+
+        /* @var $loader \Composer\Autoload\ClassLoader */
+        $loader = require $config['install_dir'].'/vendor/autoload.php';
+        $loader->addPsr4("LibreNMS\Plugin\DepMap\\", dirname(__FILE__));
+    }
+}
